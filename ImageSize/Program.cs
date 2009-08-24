56519ed3478943c15a75e84d7da1cdc5fe89980e
Eugene Petrenko <eugene.petrenko@gmail.com>
2009-08-24 06:04:57 +0000

    image size calc
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/util/trunk@52 5c360b9f-cb11-f648-b5e7-fb5e491e6b3d

---
+++ ImageSize/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImageSize
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length != 1)
+      {
+        Console.Out.WriteLine("<assembly> imagePath");
+        return;
+      }
+
+      var image = Image.FromFile(args[0]);
+
+      Console.Out.WriteLine("Image: {0}", args[0]);
+      Console.Out.WriteLine("Size: {0} {1}", image.Width, image.Height);
+
+      Clipboard.SetText(string.Format("{0} {1}", image.Width, image.Height));
+    }
+  }
+}
