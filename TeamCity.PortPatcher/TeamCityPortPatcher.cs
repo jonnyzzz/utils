f1e126ef3fb355d8b3dbdd1a78cbaad394dd6f29
Eugene Petrenko <eugene.petrenko@gmail.com>
2011-05-10 16:29:02 +0000

    add teamcity port patcher
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/util/trunk@90 5c360b9f-cb11-f648-b5e7-fb5e491e6b3d

---
+++ TeamCity.PortPatcher/TeamCityPortPatcher.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace PatchTeamCityPort
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Out.WriteLine("TeamCity port patcher. Replaces default port 8111 to 8222");
+      var home = args.Length > 0 ? args[0] : Environment.CurrentDirectory;
+      home = Path.GetFullPath(home);
+
+      Console.Out.WriteLine("Patching TeamCity ports under: " + home);
+      if (!Directory.Exists(home))
+      {
+        Console.Out.WriteLine("Failed to patch TeamCity under: " + home);
+        return;
+      }
+
+      PatchTomcatConfig(home);
+      PatchAgentConfig(home);
+    }
+
+    private static void PatchTomcatConfig(string home)
+    {
+      var path = Path.Combine(home, "conf/server.xml");
+      XmlDocument doc = new XmlDocument();
+      doc.Load(path);
+
+      doc.SelectSingleNode("Server/@port").Value = "8106";
+      doc.SelectSingleNode("Server/Service/Connector/@port").Value = "8222";
+      doc.SelectSingleNode("Server/Service/Connector/@redirectPort").Value = "8444";
+
+      doc.Save(path);
+    }
+
+    public static void PatchAgentConfig(string home)
+    {
+      var path = Path.Combine(home, "buildAgent/conf/buildAgent.properties");
+
+      var text = File.ReadAllText(path);
+      text.Replace("localhost:8111", "localhost:8222");
+
+      File.WriteAllText(path, text);
+    }
+  }
+}
