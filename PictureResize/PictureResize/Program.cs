55ce28cb7fe17fb545abb29d0c90c2b50a29d916
Eugene Petrenko <eugene.petrenko@gmail.com>
2011-11-23 15:34:37 +0000

    image converter
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/util/trunk@118 5c360b9f-cb11-f648-b5e7-fb5e491e6b3d

---
+++ PictureResize/PictureResize/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PictureResize
+{
+  class Program
+  {
+    static int Main(string[] args)
+    {
+      try
+      {
+        Console.Out.WriteLine("<Convert resolution> <from folder> <to folder> <resolutionX> <resolutionY>");
+        if (args.Length != 4) return -1;
+
+        var fromPath = args.ElementAtOrDefault(0);
+        var toPath = args.ElementAtOrDefault(1);
+        var resolutionX = int.Parse(args.ElementAtOrDefault(2));
+        var resolutionY = int.Parse(args.ElementAtOrDefault(3));
+
+        foreach (var jpg in Directory.EnumerateFiles(fromPath, "*.jpg"))
+        {
+          Console.Out.WriteLine("Converting {0}", jpg);
+
+          using (var imp = Image.FromFile(jpg))
+          {
+            var bm = new Bitmap(resolutionX, resolutionY);
+            using (var g = Graphics.FromImage(bm))
+            {
+              g.DrawImage(imp, 0, 0, bm.Width, bm.Height);
+            }
+            var path = Path.Combine(toPath, Path.GetFileName(jpg));
+            bm.Save(path, ImageFormat.Jpeg);
+          }
+        }
+        return 0;
+      }
+      catch
+      {
+        Console.Out.WriteLine("Unknown error. ");
+        return -1;
+      }
+    }
+  }
+}
