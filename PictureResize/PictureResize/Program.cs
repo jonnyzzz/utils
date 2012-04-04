119e5dfacef920dbdda1fbdb5bfc4b925d2f8f29
Eugene Petrenko <eugene.petrenko@gmail.com>
2012-04-04 19:46:34 +0000

    bufgix resizer scale
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/util/trunk@130 5c360b9f-cb11-f648-b5e7-fb5e491e6b3d

--- PictureResize/PictureResize/Program.cs
+++ PictureResize/PictureResize/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace PictureResize
 {
@@ -14,13 +12,14 @@ namespace PictureResize
     {
       try
       {
-        Console.Out.WriteLine("<Convert resolution> <from folder> <to folder> <resolutionX> <resolutionY>");
+        Console.Out.WriteLine("<Convert resolution>.exe <from folder> <to folder> <max resolution>");
         if (args.Length != 4) return -1;
 
         var fromPath = args.ElementAtOrDefault(0);
         var toPath = args.ElementAtOrDefault(1);
-        var resolutionX = int.Parse(args.ElementAtOrDefault(2));
-        var resolutionY = int.Parse(args.ElementAtOrDefault(3));
+        var res = int.Parse(args.ElementAtOrDefault(2));
+        if (!Directory.Exists(toPath))
+          Directory.CreateDirectory(toPath);
 
         foreach (var jpg in Directory.EnumerateFiles(fromPath, "*.jpg"))
         {
@@ -28,7 +27,21 @@ namespace PictureResize
 
           using (var imp = Image.FromFile(jpg))
           {
-            var bm = new Bitmap(resolutionX, resolutionY);
+            int resX;
+            int resY;
+
+            if (imp.Width > imp.Height)
+            {
+              resX = res;
+              resY = (int) (1 + (imp.Height / (double) imp.Width * res));
+            }
+            else
+            {
+              resY = res;
+              resX = (int) (1 + (imp.Width / (double) imp.Height * res));
+            }
+
+            var bm = new Bitmap(resX, resY);
             using (var g = Graphics.FromImage(bm))
             {
               g.DrawImage(imp, 0, 0, bm.Width, bm.Height);
@@ -39,9 +52,9 @@ namespace PictureResize
         }
         return 0;
       }
-      catch
+      catch(Exception e)
       {
-        Console.Out.WriteLine("Unknown error. ");
+        Console.Out.WriteLine("Unknown error. " + e);
         return -1;
       }
     }
