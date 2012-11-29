4e0e4870508871ab4f9bc412d8bafc85a1d1caaf
Eugene Petrenko <eugene.petrenko@gmail.com>
2012-11-29 22:09:45 +0000

    implement basic functionality
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/util/trunk@152 5c360b9f-cb11-f648-b5e7-fb5e491e6b3d

---
+++ LingvoOnline/LingvoOnline/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace LingvoOnline
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.Windows.Forms.Label label1;
+      this.mySearch = new System.Windows.Forms.TextBox();
+      this.myWeb = new System.Windows.Forms.WebBrowser();
+      label1 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // mySearch
+      // 
+      this.mySearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.mySearch.Location = new System.Drawing.Point(102, 13);
+      this.mySearch.Name = "mySearch";
+      this.mySearch.Size = new System.Drawing.Size(563, 20);
+      this.mySearch.TabIndex = 0;
+      this.mySearch.KeyUp += new System.Windows.Forms.KeyEventHandler(this.mySearch_KeyUp);
+      // 
+      // label1
+      // 
+      label1.AutoSize = true;
+      label1.Location = new System.Drawing.Point(61, 13);
+      label1.Name = "label1";
+      label1.Size = new System.Drawing.Size(33, 13);
+      label1.TabIndex = 1;
+      label1.Text = "Wort:";
+      // 
+      // myWeb
+      // 
+      this.myWeb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.myWeb.Location = new System.Drawing.Point(12, 39);
+      this.myWeb.MinimumSize = new System.Drawing.Size(20, 20);
+      this.myWeb.Name = "myWeb";
+      this.myWeb.Size = new System.Drawing.Size(656, 399);
+      this.myWeb.TabIndex = 2;
+      this.myWeb.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.myWeb_DocumentCompleted);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(680, 450);
+      this.Controls.Add(this.myWeb);
+      this.Controls.Add(label1);
+      this.Controls.Add(this.mySearch);
+      this.MinimumSize = new System.Drawing.Size(500, 300);
+      this.Name = "Form1";
+      this.Text = "Lingvo.Online translate";
+      this.Activated += new System.EventHandler(this.Form1_Activated);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox mySearch;
+    private System.Windows.Forms.WebBrowser myWeb;
+  }
+}
+
---
+++ LingvoOnline/LingvoOnline/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using mshtml;
+
+namespace LingvoOnline
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private string SearchText
+    {
+      get { return mySearch.Text.Trim(); }
+      set { mySearch.Text = (value ?? "").Trim(); }
+    }
+
+    private void UpdateSearch()
+    {
+      myWeb.Navigate("http://lingvopro.abbyyonline.com/ru/Translate/de-ru/" + SearchText);
+    }
+
+    private void Form1_Activated(object sender, EventArgs e)
+    {
+      var text = (Clipboard.GetText() ?? "").Trim();
+      if (!string.IsNullOrWhiteSpace(text) && !text.Equals(SearchText))
+      {
+        SearchText = text;
+        UpdateSearch();
+      }
+    }
+
+    private void mySearch_KeyUp(object sender, KeyEventArgs e)
+    {
+      if (e.KeyCode == Keys.Enter)
+      {
+        UpdateSearch();
+      }
+    }
+
+    private void myWeb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+    {
+      dynamic doc = myWeb.Document.DomDocument;
+      if (doc == null) return;
+
+      foreach (string clazz in new[] { "b-comments-facebook", "b-user-links", "b-glossary", "b-head__r-col", "home", "mainmenu", "topmenu", "b-right-panel", "js-correct-height", "b-useful-info-after-searchform" })
+      {
+        foreach (dynamic o in doc.getElementsByClassName(clazz))
+        {
+          o.parentNode.removeChild(o);
+        }
+      }
+
+    }
+  }
+}
